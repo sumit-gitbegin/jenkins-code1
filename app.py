98c5e5922953e4c875af7326c4a1c99dfaa77405
sumit-gitbegin <71128512+sumit-gitbegin@users.noreply.github.com>
2026-05-15 13:53:09 +0530

    Rename home function to hello and update response

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Sample App!"
+def hello():
+    return "Hello from Jenkins Docker Deployment!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
